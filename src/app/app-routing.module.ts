291e890e0d0be37572e7f4c77d8d982ec13fefb4
chinemerem2525 <chubexchange@gmail.com>
2025-07-19 00:11:11 +0100

    ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectsComponent } from './client/projects/projects.component';
+import { RealestateProjectDetailsComponent } from './client/realestate-project-details/realestate-project-details.component';
+import { RemovalCompanyProjectDetailsComponent } from './client/removal-company-project-details/removal-company-project-details.component';
+import { CyberSecurityProjectDetailsComponent } from './client/cyber-security-project-details/cyber-security-project-details.component';
 
 const routes: Routes = [
        {
     path: '',
     loadChildren: ()=> import('./client/client.module').then(m => m.ClientModule),
-  }
+  },
+   {
+          path: 'projects',
+          component: ProjectsComponent
+        },
+        {
+          path: 'realestate-project-details',
+          component: RealestateProjectDetailsComponent
+        },
+        {
+          path: 'removal-company-project-details',
+          component: RemovalCompanyProjectDetailsComponent
+        },
+        {
+          path: 'cyber-security-project-details',
+          component: CyberSecurityProjectDetailsComponent
+        }
 ];
 
 @NgModule({
